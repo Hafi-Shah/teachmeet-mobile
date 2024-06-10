d8b243cd51d3cec5124e199c3f13a0e3d251dc68
hafi <devhafi1337@gmail.com>
2024-06-10 09:48:39 +0500

    new

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,20 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular'; // Import IonicModule from Ionic
-import { NgprimeModule } from './widgets/ngprime.module';
-import { MeetDialogComponent } from './Dialogs/meet-dialog/meet-dialog.component';
 
 @NgModule({
-  declarations: [
-    MeetDialogComponent
-  ],
-  exports: [ReactiveFormsModule, FormsModule, IonicModule, NgprimeModule], // Export IonicModule
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    FormsModule,
-    IonicModule,
-    NgprimeModule,
-  ],
+  declarations: [],
+  exports: [ReactiveFormsModule, FormsModule, IonicModule], // Export IonicModule
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, IonicModule], // Include IonicModule
 })
 export class SharedModule {}
